68288cba17577ca2c4430fcc02bc8be12e2d2b3b
Ajinkya Borade <ajinkyab1987@gmail.com>
2013-12-08 17:56:56 +0530

    Updated page. css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+(function(){
+    $('.tab_links li a').on('click', function(){
+        $('i.arrow').remove();
+        $('.selected').removeClass('selected');
+        $(this).after('<i class="arrow"></i>').addClass('selected');
+    });
+})();
