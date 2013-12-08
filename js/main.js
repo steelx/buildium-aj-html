45251b48cd5b53d9462ee1724382cb88a72b6879
Ajinkya Borade <ajinkyab1987@gmail.com>
2013-12-08 21:54:27 +0530

    Updated tabs JQ script

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 (function(){
-    $('.tab_links li a, .tabs li a').on('click', function(){
-        $('i.arrow').remove();
-        $('.selected').removeClass('selected');
+    $('.tab_links li a').on('click', function(){
+        $('.tab_links').find('i.arrow').remove();
+        $('.tab_links').find('a.selected').removeClass('selected');
         $(this).append('<i class="arrow"></i>').addClass('selected');
     });
 
     $('.tabs li a').on('click', function(e){
+        $('.tabs').find('i.arrow').remove();
+        $('.tabs').find('a.selected').removeClass('selected');
+        $(this).append('<i class="arrow"></i>').addClass('selected');
+
         var $hash = $($(this).attr('href'));
         console.log($hash);
         $('.tabs_content').hide();
         $hash.fadeIn(200);
         e.preventDefault();
     });
+    $('.tabs li a.selected').trigger('click');
 
 })();
